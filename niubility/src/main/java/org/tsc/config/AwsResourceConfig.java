e3a362ee26384371cc3788948c814f56677c36c8
ZJAllenJiang <weijiang@bu.edu>
2023-09-22 13:35:36 +0800

    Add docker compose

--- niubility/src/main/java/org/tsc/config/AwsResourceConfig.java
+++ niubility/src/main/java/org/tsc/config/AwsResourceConfig.java
@@ -1,8 +1,20 @@
 package org.tsc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AwsResourceConfig {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*"); // adjust as necessary
+            }
+        };
+    }
 }
